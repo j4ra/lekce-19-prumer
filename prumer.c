92218b0278b20fdf46e1d66c305ca92324b0bd86
Jaroslav Nejedly <jardanejedly@hotmail.com>
2014-02-19 15:32:47 +0100

    Smerodatna odchylka

--- prumer.c
+++ prumer.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #define EPSILON 0.0001
 
 int je_nula(double x) {
 	return (x < EPSILON) && (x > -EPSILON);
+}
+
+double smerodatna_odchylka(double prumer, double* cisla, int pocet){
+	double suma_mocnin = 0.;
+	for(int i = 0; i < pocet; i++){
+		suma_mocnin += fabs(prumer - cisla[i]) * fabs(prumer - cisla[i]);
+	}
+	return sqrt(suma_mocnin / pocet);
 }
 
 int main() {
@@ -42,6 +51,7 @@ int main() {
 	}
 	
 	printf("Prumer je %lf\n", suma / pocet);
+	printf("Smerodatna odchylka je %lf\n", smerodatna_odchylka(suma / pocet, cisla, pocet));
 	
 	return 0;
 }
